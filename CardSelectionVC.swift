17b79577ca4f0da031a4ceac642eb28f0a7ff344
hanhqvu <hanhqvu@users.noreply.github.com>
2023-01-09 00:17:27 +0900

    fea: implement stop & restart

--- CardSelectionVC.swift
+++ CardSelectionVC.swift
@@ -19,11 +19,25 @@ class CardSelectionVC: UIViewController {
         startTimer()
     }
     
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        timer.invalidate()
+    }
+    
     func startTimer() {
         timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(showRandomImage), userInfo: nil, repeats: true)
     }
     
     @objc func showRandomImage() {
         cardImageView.image = cards.randomElement() ?? UIImage(named: "AS")
+    }
+    
+    @IBAction func stopButtonTapped(_ sender: Any) {
+        timer.invalidate()
+    }
+    
+    @IBAction func restartButtonTapped(_ sender: Any) {
+        timer.invalidate()
+        startTimer()
     }
 }
