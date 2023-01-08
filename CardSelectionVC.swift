df86db1a9f42f2eaba076313fb88c44bef3cb18b
hanhqvu <hanhqvu@users.noreply.github.com>
2023-01-09 00:10:42 +0900

    feat: show random images on timer

--- CardSelectionVC.swift
+++ CardSelectionVC.swift
@@ -9,7 +9,21 @@ import UIKit
 
 class CardSelectionVC: UIViewController {
     
+    @IBOutlet var cardImageView: UIImageView!
+    var cards: [UIImage] = Deck.allValues
+    
+    var timer: Timer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        startTimer()
+    }
+    
+    func startTimer() {
+        timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(showRandomImage), userInfo: nil, repeats: true)
+    }
+    
+    @objc func showRandomImage() {
+        cardImageView.image = cards.randomElement() ?? UIImage(named: "AS")
     }
 }
